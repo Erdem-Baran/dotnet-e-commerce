01120090a7f0a110ef9cc58a4f99ed31d1e0a8a0
Erdem-Baran <erdembaran638@gmail.com>
2026-01-27 19:43:49 +0300

    ""

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,6 +5,6 @@ public class Product
     public int Id { get; set; }
     public string ProductName { get; set; } = null!;
     public double Price { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActiv { get; set; }
     public bool HomePage { get; set; }
 }
