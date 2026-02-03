7a0624f4117c08ce9ac9a8e4c12fd717959044c3
Erdem-Baran <erdembaran638@gmail.com>
2026-02-03 07:33:17 +0300

    "add migrations"

--- obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
+++ obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304a94001aba903e630cda282d274cef35f8d95f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6213099d71e586104fc648fa7d742d7d820bdfd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
