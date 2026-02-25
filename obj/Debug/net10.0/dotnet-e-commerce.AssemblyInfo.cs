e6314a24524c736cc405f7cedcfa0195eb2d51fc
Erdem-Baran <erdembaran638@gmail.com>
2026-02-25 17:04:57 +0300

    deleted db

--- obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
+++ obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c59743f63d70fb07d19bcfa8dc87c6137d2d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e982ef58c68003b5f59d6df2b62e7e77b00c3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
