7751ea6e4375a14eb13e30e2d92b059b09069bc2
Erdem-Baran <erdembaran638@gmail.com>
2026-03-21 11:25:35 +0300

    category model

--- obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
+++ obj/Debug/net10.0/dotnet-e-commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b05140e67658578d7d50ccb962b81d966f410b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59be1e4218538c99fd255a175006889ba93fa93a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
