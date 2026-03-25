e3d6b8cfad36967e5eed377c8cbbbf6b15050a29
Erdem-Baran <erdembaran638@gmail.com>
2026-03-25 06:39:59 +0300

    product table has been completed

--- Models/Kategori.cs
+++ Models/Kategori.cs
@@ -6,5 +6,5 @@ public class Kategori
     public int Id { get; set; }
     public string KategoriAdi { get; set; } = null!;
     public string Url { get; set; } = null!;
-    public List<Urun> Uruns { get; set; } = new();  // navigation property
+    public List<Urun> Uruns { get; set; } = new(); // navigation property
 }
